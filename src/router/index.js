6ab9663e172cde8cd1e68cf01539023cbfb9cc95
yuchengyi520 <315484255@qq.com>
2020-06-29 14:42:16 +0800

    登陆页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+//import Home from "../views/Home.vue";
+//import Login from "../views/Login/index.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login/index.vue")
   },
   {
     path: "/about",
